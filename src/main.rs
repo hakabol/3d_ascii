d94528d889c7e2a77c7c197c4dd702317c1ab361
hakabol <habelcijuanu@gmail.com>
2026-05-07 16:30:39 +0530

    final commit of beta version

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,11 @@ fn main(){
     let mut a = 0.0;
     let mut b = 0.0;
 
-    let torus = ascii_3d::torus(100.0, 30.0);
-    let cube = ascii_3d::cube(100, [100.0, 0.0, 100.0]);
-
-    let screen = ascii_3d::combine(&[cube, torus]);
-
     loop{
+        let torus = ascii_3d::torus(100.0, 30.0);
+        let cube = ascii_3d::cube(100, [100.0, 0.0, 100.0]);
+
+        let screen = ascii_3d::combine(&[cube, torus]);
         ascii_3d::plot(&screen, a, b, [255, 255, 255], 250.0, [0.5, 0.5, -1.0, 0.5, 0.5]);
 
         a += 0.03;
